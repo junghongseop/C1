807263ae1bbc8473b12d3180cefbe2c333405e18
정홍섭 <a01082372487@gmail.com>
2026-03-27 09:04:38 +0900

    Initial Commit

---
+++ C1/C1App.swift
@@ -0,0 +1,17 @@
+//
+//  C1App.swift
+//  C1
+//
+//  Created by 정홍섭 on 3/27/26.
+//
+
+import SwiftUI
+
+@main
+struct C1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
