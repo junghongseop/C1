b5e0cb5023fff3abde3d702dff184ffbd14645ae
정홍섭 <a01082372487@gmail.com>
2026-04-17 16:19:56 +0900

    feat: 앱내 커스텀 버튼 스타일 추가

--- C1/Views/Components/Buttons/PrimaryButtonStyle.swift
+++ C1/Views/Components/Buttons/PrimaryButtonStyle.swift
@@ -5,4 +5,18 @@
 //  Created by 정홍섭 on 4/17/26.
 //
 
-import Foundation
+import SwiftUI
+
+struct PrimaryButtonStyle: ButtonStyle {
+    @Environment(\.isEnabled) private var isEnabled
+    
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .frame(maxWidth: .infinity, maxHeight: 50)
+            .background(isEnabled ? Color.accentColor : Color.blue.opacity(0.5))
+            .foregroundStyle(isEnabled ? .white: .white.opacity(0.5))
+            .cornerRadius(14)
+            .fontWeight(.bold)
+            .opacity(configuration.isPressed ? 0.8 : 1)
+    }
+}
