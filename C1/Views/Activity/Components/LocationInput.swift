65372f16a71e07dd2dcfb45924fc2b9f8137cf72
정홍섭 <a01082372487@gmail.com>
2026-04-17 16:23:56 +0900

    feat: 장소명 인풋 컴포넌트 구현

--- C1/Views/Activity/Components/LocationInput.swift
+++ C1/Views/Activity/Components/LocationInput.swift
@@ -8,11 +8,37 @@
 import SwiftUI
 
 struct LocationInput: View {
+    var isFocused: FocusState<Bool>.Binding
+    
+    @State var location: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 12) {
+            Text("장소명")
+                .foregroundStyle(.white)
+                .font(.system(size: 20, weight: .heavy))
+            
+            TextField("장소를 입력해주세요.", text: $location)
+                .font(.callout)
+                .focused(isFocused)
+                .padding(12)
+                .frame(height: 44)
+                .background(Color.white)
+                .clipShape(RoundedRectangle(cornerRadius: 8))
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .inset(by: isFocused.wrappedValue ? 0.5 : 0)
+                        .stroke(
+                            isFocused.wrappedValue ? Color("mediumLightBlue") : Color.clear,
+                            lineWidth: isFocused.wrappedValue ? 2 : 0
+                        )
+                )
+        }
     }
 }
 
 #Preview {
-    LocationInput()
+    @FocusState var isFocused: Bool
+    
+    LocationInput(isFocused: $isFocused)
 }
