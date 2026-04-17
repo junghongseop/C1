26d3cd0e5fa8e8911bbe729c90af0b5772e73997
정홍섭 <a01082372487@gmail.com>
2026-04-17 16:24:22 +0900

    feat: 날짜 입력 DatePicker 커스텀 컴포넌트 구현

--- C1/Views/Activity/Components/ActivityDatePicker.swift
+++ C1/Views/Activity/Components/ActivityDatePicker.swift
@@ -8,8 +8,26 @@
 import SwiftUI
 
 struct ActivityDatePicker: View {
+    @State var selectedDate = Date()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 12) {
+            Text("날짜")
+                .foregroundStyle(.white)
+                .font(.system(size: 20, weight: .heavy))
+            
+            DatePicker(
+                "날짜 선택",
+                selection: $selectedDate,
+                in: ...Date(),
+                displayedComponents: [.date]
+            )
+            .datePickerStyle(.compact)
+            .clipShape(RoundedRectangle(cornerRadius: 8))
+            .environment(\.locale, Locale(identifier: "ko_KR"))
+            .labelsHidden()
+            .tint(Color.black)
+        }
     }
 }
 
